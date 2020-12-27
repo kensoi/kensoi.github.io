d9d6e5595b39ec39a191bd0ff910f26ccae545a3
Andrew Prokofieff <prokofieff.help@gmail.com>
2020-12-27 13:06:40 +0500

    Design update
    - design update
    - clear pages deleted, now it redirects to 404 error

--- js/redirection.js
+++ js/redirection.js
@@ -1,31 +1,27 @@
 function redirect(redirected) {
-    link = 'http://kensoi.github.io/'
+    link = 'https://kensoi.github.io/'
 
     switch(redirected) {
         case 1: 
-            link = 'http://github.com/kensoi'; break;
+            link = 'https://github.com/kensoi'; break;
         case 2: 
-            link = 'http://t.me/kensoi_fuji'; break;
+            link = 'https://t.me/kensoi_fuji'; break;
         case 3: 
-            link = 'http://vk.com/kensoi'; break;
+            link = 'https://vk.com/kensoi'; break;
         case 4: 
-            link += 'projects'; break;
-        case 5: 
-            link += 'projects/#sources'; break;
-        case 6: 
-            link += 'design'; break;
-        case 7: 
             link = 'https://drive.google.com/drive/folders/1Lh5jXPR9IHGoBQgC9C0nRUTxvW5ubagt'; break;
-        case 8: 
-            link = 'http://github.com/kensoi/testcanarybot/'; break;
-        case 9: 
-            link = 'http://github.com/kensoi/apigram/'; break;
-        case 10: 
-            link = 'http://github.com/kensoi/complex_angle/'; break;
-        case 11: 
+        case 5: 
+            link += 'design.html'; break;
+        case 6: 
+            link += 'contacts.html'; break;
+        case 7: 
             link += 'canarybot'; break;
-        case 12: 
+        case 8: 
             link += 'testcanarybot'; break;
+        case 9: 
+            link += 'canarybot/install'; break;
+        case 10: 
+            link += 'canarybot/rules'; break;
     };
     location.href = link
 }
